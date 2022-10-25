2748b64c1c1fb1074b130a6cbf1ca158a52d0bd2
Ambar <Ambarpg80@gmail.com>
2022-10-24 20:26:54 -0400

    Add elements to index.html, add styling to style.css, finish configuring first event listener to index.js

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 
 const cards =  document.querySelector('.card');
-//let ul = document.createElement('ul');
-//let li = document.createElement('li')
-//cards.append(div);
 
-// function toggle(){
-// li.classList.toggle("changemsg") 
-// document.getElementsByClassName("changemsg").innertext = `${eachSvc.description} `
-// }
 
-function showCards(services){
+function showMsg(services){
     for (let eachSvc of services){
         let div = document.createElement('div');
-        div.className = "changemsg";
+        div.className = "cells","changemsg";
+        div.innerHTML = `<span>${eachSvc.name} </span> 
+                        <img src=${eachSvc.image_url}>
+                        <style> img{
+                            width : 300px; 
+                            height :300px;
+                            border: 0.5px #ebb987 solid;
+                        }</style>`  
+        
         div.addEventListener("mouseover",(e)=>{
             e.target.classList.toggle("changemsg") 
-            document.getElementsByClassName("changemsg").innertext = `${eachSvc.description} `
-        })
-        div.innerHTML = `<span>${eachSvc.name} </span> 
-                        <img src=${eachSvc.image_url}
-                        width = "250px", 
-                        height = "250px">
-                        `
-        //ul.append(li)
+            div.innerHTML = `<span class= "secondmessage"> ${eachSvc.description} </span>
+                            <style> img {display : block};  </style> `;  
+         })
+         div.addEventListener("mouseout",()=>{
+            div.innerHTML = `<span>${eachSvc.name} </span> 
+                        <img src=${eachSvc.image_url}>
+                        <style> img{
+                            width : 300px; 
+                            height :300px;
+                            border: 0.5px #ebb987 solid;
+                        }</style>`      
+         })
         cards.append(div);
         
     }
 }
+document.getElementById("main-container")
+
 
 
 document.addEventListener('DOMContentLoaded',
 fetch("http://localhost:3000/services")
 .then((response) => response.json())
-.then((services)=> showCards(services))
+.then((services)=> showMsg(services))
 )
